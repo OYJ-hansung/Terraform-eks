ac6189d97c7af79e18e248a242f64d153c122fcb
YoungJoo <87402175+OYJ-hansung@users.noreply.github.com>
2022-07-27 16:47:32 +0900

    add docker engine install / add jenkins user to docker group

--- instance.tf
+++ instance.tf
@@ -24,7 +24,20 @@ resource "aws_instance" "buildsvr" {
                 sudo apt-get update -y && sudo apt-get install -y openjdk-11-jre
                 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
                 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-                sudo apt-get update -y && sudo apt-get install jenkins -y         
+                sudo apt-get update -y && sudo apt-get install jenkins -y
+                sudo apt-get update
+                sudo apt-get install ca-certificates curl gnupg lsb-release
+                sudo mkdir -p /etc/apt/keyrings
+                curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+                echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+                apt-cache madison docker-ce
+                sudo apt-get install docker-ce=5:20.10.16~3-0~ubuntu-focal docker-ce-cli=5:20.10.16~3-0~ubuntu-focal containerd.io docker-compose-plugin
+                sudo gpasswd -a jenkins docker
+                sudo systemctl daemon-reload
+                sudo systemctl restart docker
+                sudo service jenkins restart
                 EOF
   tags = {
     Name = "Build Server"
