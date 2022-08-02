d086773210c6e49850f114c56e85743638f8418e
YoungJoo <87402175+OYJ-hansung@users.noreply.github.com>
2022-08-03 01:36:15 +0900

    double quote escape error fixed

--- local.tf
+++ local.tf
@@ -24,7 +24,8 @@ resource "null_resource" "this" {
   }
     
   provisioner "local-exec" {
-    command = "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+    command = "kubectl patch svc argocd-server -n argocd -p '{\"spec\": {\"type\": \"LoadBalancer\"}}'
+"
   }
   
   depends_on = [
