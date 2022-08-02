6154fe73ed602ee7daeb0e80a361ea0d14b147ed
YoungJoo <87402175+OYJ-hansung@users.noreply.github.com>
2022-08-03 00:40:31 +0900

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "GoormEKSClusterSG" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "GoormEKSClusterSG"
