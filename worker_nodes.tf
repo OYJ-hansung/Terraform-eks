87e4dcc5a82be5ee0079529da65dd7e2a33dc654
YoungJoo <87402175+OYJ-hansung@users.noreply.github.com>
2022-08-04 00:03:45 +0900

    Update worker_nodes.tf

--- worker_nodes.tf
+++ worker_nodes.tf
@@ -19,6 +19,7 @@ resource "aws_eks_node_group" "eks-nodes-t2" {
 
   remote_access {
     source_security_group_ids = [aws_security_group.GoormEKSClusterSG.id]
+    ec2_ssh_key               = "key0722"
   }
   
   depends_on = [
